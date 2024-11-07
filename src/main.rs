0cd89b8e9ebadcbe6cace03649a936614ee1f97a
CIOSAI <jyinteractive.tw@gmail.com>
2024-11-08 01:48:02 +0800

    setup

---
+++ src/main.rs
@@ -0,0 +1,31 @@
+use cen::app::app::AppConfig;
+use kiyo::app::App;
+use kiyo::app::draw_orch::{ClearConfig, DispatchConfig, DrawConfig, ImageConfig, Pass};
+
+fn main() {
+
+    let app = App::new(AppConfig {
+        width: 1920 / 2,
+        height: 1080 / 2,
+        vsync: true,
+        log_fps: false,
+    });
+
+    let config = DrawConfig {
+        images: Vec::from([
+            ImageConfig {
+                clear: ClearConfig::Color(0.0,0.0,0.0)
+            },
+        ]),
+        passes: Vec::from([
+            Pass {
+                shader: "src/shaders/main.comp".to_string(),
+                dispatches: DispatchConfig::FullScreen,
+                input_resources: Vec::from([]),
+                output_resources: Vec::from([ 0 ]),
+            },
+        ])
+    };
+
+    app.run(config, None);
+}
